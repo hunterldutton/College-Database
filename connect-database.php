867196264521230b0d37c984756bafdb7751b8a3
Hunter Dutton <77072722+hunterldutton@users.noreply.github.com>
2024-09-20 14:35:59 -0400

    Delete connect-database.php

--- connect-database.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbFaculty = 'HDUTTON_cs120';
-$dsn = 'mysql:host=webdb.uvm.edu;dbname='.$dbFaculty;
-$username = 'hdutton_writer';
-$password = 'am9YHGaOxrjq7OiH';
-
-$pdo = new PDO($dsn, $username, $password);
-?>
